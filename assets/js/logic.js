55912d12f7a8c8fb6b1daf93ac677c1570e81bb9
1curtislee <1curtislee@gmail.com>
2019-07-23 13:40:34 -0700

    contact form

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,22 @@
-$("#contactButton").click(function(){
-  console.log("contact button clicked")
-  
-});
+
 
 $(document).ready(function(){
   $('.modal').modal();
+});
+
+$('#contactSubmit').on('click', function(){
+  console.log('contact form coming soon...')
+
+  const sgMail = require('@sendgrid/mail');
+  // 'require' problem, require must be in a node file (like server.js)
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: 'curtishumphrey1@gmail.com',
+    from: $('#last_name').val(),
+    subject: 'email from portfolio website',
+    text: $('textarea1').val(),
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg);
 });
