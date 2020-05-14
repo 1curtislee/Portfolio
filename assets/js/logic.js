0a31897c8d6cbffdb0c727927a65cc54b17d310d
1curtislee <1curtislee@gmail.com>
2020-05-14 15:54:06 -0700

    testing sendgrid email api

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,22 +1,9 @@
-
-
 $(document).ready(function(){
   $('.modal').modal();
 });
 
-$('#contactSubmit').on('click', function(){
+$('#contactForm').on('submit', function(){
   console.log('contact form coming soon...')
 
-  const sgMail = require('@sendgrid/mail');
-  // 'require' problem, require must be in a node-run file (like server.js)
-
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = {
-    to: 'curtishumphrey1@gmail.com',
-    from: $('#last_name').val(),
-    subject: 'email from portfolio website',
-    text: $('textarea1').val(),
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  };
-  sgMail.send(msg);
+  
 });
