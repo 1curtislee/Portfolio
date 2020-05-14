4df89c4972332eb87883c6a10be1b7772d4a2bab
1curtislee <1curtislee@gmail.com>
2020-05-14 15:14:50 -0700

    added to index.php logic for contact form to send email to curtishumphrey1@gmail.com

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php include_once("index.html"); ?>
+<?php
+
+if($_POST["submit"]) {
+  $recipient="curtishumphrey1@gmail.com";
+  $subject="contact message from Portfolio";
+  $sender=$_POST["email"];
+  $message=$_POST["message"];
+
+  mail($recipient, $subject, $_POST["message"], "From: $sender");
+}
+
+include_once("index.html");
