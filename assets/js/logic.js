900767062476d4afa8a35984ac667990d9bfa00f
1curtislee <1curtislee@gmail.com>
2020-05-14 16:39:45 -0700

    testing sendgrid email

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
   $('.modal').modal();
 });
+
+const contactEmail = (event) => {
+  
+  console.log('contactEmail(); run');
+  let sender = document.getElementById('email');
+  console.log(sender);
+
+  const sgMail = require('@sendgrid/mail');
+  // using Twilio SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: process.env.RECIPIENT,
+    from: sender,
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail.send(msg);
+  event.preventDefault();
+}
--- server.js
+++ server.js
@@ -7,19 +7,25 @@ http.createServer((req, res) => {
   file.serve(req, res);
 }).listen(8080)
 
-let sender = document.getElementById();
 
-const contactEmail = () => {
+
+const contactEmail = (event) => {
+  
+  console.log('contactEmail(); run')
+  let sender = document.getElementById('email');
+  console.log(sender);
+
   const sgMail = require('@sendgrid/mail');
   // using Twilio SendGrid's v3 Node.js Library
   // https://github.com/sendgrid/sendgrid-nodejs
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: process.env.RECIPIENT,
-    from: 'test@example.com',
+    from: sender,
     subject: 'Sending with Twilio SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
     html: '<strong>and easy to do anywhere, even with Node.js</strong>',
   };
   sgMail.send(msg);
+  event.preventDefault();
 }
