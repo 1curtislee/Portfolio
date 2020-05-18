b14e04353fd4d20a7603c3e1a7e016a4351f5a55
1curtislee <1curtislee@gmail.com>
2020-05-18 14:56:10 -0700

    testing contact form sendgrid integration

--- server.js
+++ server.js
@@ -1,31 +1,62 @@
-const static = require('node-static');
-const http = require('http');
+require('dotenv').config();
+const path = require('path');
+const express =  require('express');
+const helmet = require('helmet'); // helps set headers, https://www.npmjs.com/package/helmet
+const PORT = process.env.PORT || 3000;
 
-const file = new(static.Server)();
+const app = express();
+app.use(helmet());
+app.use(express.urlencoded({ extended: false })); 
+app.use(express.static(path.join(__dirname))); // uses static file
+app.use(express.json());
 
-http.createServer((req, res) => {
-  file.serve(req, res);
-}).listen(8080)
+app.use((req, res, next) => { // not clear on what this section does...
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+	next();
+});
 
+app.options('*', function(req, res) {
+	res.send(200);
+});
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
 
-const contactEmail = (event) => {
-  
-  console.log('contactEmail(); run')
-  let sender = document.getElementById('email');
-  console.log(sender);
+// contact form email handler
+app.post('/email', (req, res) => {
+  let sender = req.body.formName;
+  let email = req.body.formEmail;
+  let message = req.body.formMessage;
 
-  const sgMail = require('@sendgrid/mail');
   // using Twilio SendGrid's v3 Node.js Library
   // https://github.com/sendgrid/sendgrid-nodejs
+  const sgMail = require('@sendgrid/mail');
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
   const msg = {
     to: process.env.RECIPIENT,
-    from: sender,
-    subject: 'Sending with Twilio SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    from: email,
+    subject: 'contact form submission from ' + sender,
+    text: message,
   };
-  sgMail.send(msg);
-  event.preventDefault();
-}
+  sgMail
+  .send(msg)
+  .then(() => {}, error => {
+    console.log(error);
+
+    console.error(error);
+    if (error.response) {
+      console.error(error.response.body)
+    }
+  });
+
+  res.redirect('/');
+});
+
+app.listen(PORT, (err) => {
+  if (err) { throw err; }
+  console.log('server running on port '+ PORT);
+});
